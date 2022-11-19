b95722ffd851cc0bca64b821568da75dfe938d47
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:39:19 +0800

    Update metaphorpsum.py

--- src/metaphorpsum.py
+++ src/metaphorpsum.py
@@ -1,19 +1,19 @@
 from requests import get
 
 class MetaphorpSum:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "http://metaphorpsum.com"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 
-	def get_sentences(self, count: int):
+	def get_sentences(self, count: int) -> str:
 		return get(
 			f"{self.api}/sentences/{count}",
 			headers=self.headers).text
 
-	def get_paragraphs(self, count: int):
+	def get_paragraphs(self, count: int) -> str:
 		return get(
 			f"{self.api}/paragraphs/{count}",
 			headers=self.headers).text
@@ -21,7 +21,7 @@ class MetaphorpSum:
 	def get_paragraphs_with_sentences(
 			self,
 			paragraphs_count: int,
-			sentences_count: int):
+			sentences_count: int) -> str:
 		return get(
 			f"{self.api}/paragraphs/{paragraphs_count}/sentences_count",
 			headers=self.headers).text
